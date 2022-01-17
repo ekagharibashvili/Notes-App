fa113653932b1dceb2d1cc8fac0658f23ca4b93b
Eka Garibashvoili <EkaGaribashvili98@gmail.com>
2022-01-17 20:01:26 +0400

    refactor: refactor function declarations to arrow function

--- notes.js
+++ notes.js
@@ -1,16 +1,14 @@
 const fs = require("fs")
 const chalk = require("chalk")
 
-const getNotes = function () {
+const getNotes = () => {
     return 'Your notes...'
 }
 
 
-const removeNote = function (title) {
+const removeNote = (title) => {
     const notes = loadNotes()
-    let newNotes = notes.filter(function (note) {
-        return note.title !== title
-    })
+    let newNotes = notes.filter((note) => note.title !== title)
     if (notes.length > newNotes.length) {
         console.log(chalk.green.inverse('Note removed!'))
         saveNotes(newNotes)
@@ -20,11 +18,9 @@ const removeNote = function (title) {
     }
 }
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -40,12 +36,12 @@ const addNote = function (title, body) {
 
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
